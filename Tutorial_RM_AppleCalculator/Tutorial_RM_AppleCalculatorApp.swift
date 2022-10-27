bb5eacce8f4164e3cb9f80cde4f02ac43bb71a46
Sedat Onat <sedatonat@outlook.com>
2022-10-27 12:29:19 +0300

    Initial Commit

---
+++ Tutorial_RM_AppleCalculator/Tutorial_RM_AppleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_RM_AppleCalculatorApp.swift
+//  Tutorial_RM_AppleCalculator
+//
+//  Created by Sedat Onat on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_RM_AppleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
